b1f6604c90780bdbe1b061d138010240d0711411
yaminlee <naamail@post.bgu.ac.il>
2021-11-21 22:30:08 +0200

    exersize5

---
+++ exersize5_java.js
@@ -0,0 +1,32 @@
+
+
+function namefunction(){
+   // var String x  ;
+   // x=sapir-bibi;
+    
+    
+    //console.log(x);
+   // document.getElementById("demo2").innerHTML= end;
+    //console.log(bool);
+    
+    
+   // var user = {
+    //    firstname: 'eli', 
+    //    lastname: 'cohen' ,
+    //    email: 'elicohen@gmail.com',
+    //    age:25
+    //}
+  //  console.log(user);
+   // console.log(user.email);
+    //}
+
+
+    
+var user = {
+    firstname: 'sapir', 
+    lastname: 'bibi' ,
+   
+}
+console.log(user);
+
+}
---
+++ jsintro.js
@@ -0,0 +1,32 @@
+
+//function click() {
+  //  console.log('hi function');
+  //  document.getElementById("demo").innerHTML="hi js function!";
+//}
+
+function myfunction(){
+var x , y , z ;
+x=7;
+y=5;
+z=x+y;
+
+var end ;
+end = z**y;
+
+var bool;
+bool=(x==y);
+
+console.log(end);
+document.getElementById("demo2").innerHTML= end;
+console.log(bool);
+
+
+var user = {
+    firstname: 'eli', 
+    lastname: 'cohen' ,
+    email: 'elicohen@gmail.com',
+    age:25
+}
+console.log(user);
+console.log(user.email);
+}
