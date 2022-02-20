76b1b369fa89081290431ada214388e6f74824fa
sapirxpike <93188353+sapirxpike@users.noreply.github.com>
2022-02-20 16:24:19 +0200

    Update app.py

--- arseni/app.py
+++ arseni/app.py
@@ -90,6 +90,90 @@ def user_func():
 
 
 
+assignment10 = Blueprint('assignment10', __name__,
+                         static_folder='static',
+                         template_folder='templates')
+
+@assignment10.route('/assignment10')
+def assignment10_func():
+    query = 'select * from users;'
+    users = interact_db(query=query, query_type='fetch')
+    return render_template('assignment10.html', users=users)
+
+
+@assignment10.route('/insert_user', methods=['POST'])
+def insert_user_func():
+    session.clear()
+    # get the data
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    # validation
+    # insert to db
+    if first_name != "" and last_name != "" and email != "":
+        query = "insert into users(first_name, last_name, email) VALUES('%s', '%s', '%s');" % (first_name, last_name, email)
+        interact_db(query=query, query_type='commit')
+        session['insert'] = True
+        # come back to users
+        return redirect('/assignment10')
+    session['insert'] = False
+    return redirect('assignment10')
+
+
+@assignment10.route('/update_user', methods=['POST'])
+def update_user_func():
+    session.clear()
+    user_old_email = request.form['old_email']
+    user_email = request.form['new_email']
+    user_fname = request.form['new_fname']
+    user_lname = request.form['new_lname']
+    # validation
+    if user_email != "" and user_fname != "" and user_lname != "":
+        query = "UPDATE users SET email='%s', first_name='%s', last_name='%s' WHERE email='%s';" % (user_email, user_fname, user_lname, user_old_email)
+        interact_db(query=query, query_type='commit')
+        session['update'] = True
+        return redirect('/assignment10')
+    session['update'] = False
+    return redirect('assignment10')
+
+@assignment10.route('/delete_user',  methods=['POST'])
+def delete_user_func():
+    session.clear()
+    user_email = request.form['email']
+    # validation
+    if user_email != "":
+        query = "delete from users where email='%s';" % user_email
+        interact_db(query=query, query_type='commit')
+        session['delete'] = True
+        return redirect('/assignment10')
+    session['delete'] = False
+    return redirect('/assignment10')
+
+
+
+
+
+@app.route('/logout')
+def logout_func():
+    session['username'] = ''
+    return render_template('CVgrid-exersize5.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_func():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # DB
+        found = True
+        if found:
+            session['username'] = username
+            return redirect(url_for('aboutme_func'))
+        else:
+            return render_template('login.html')
+
 
 
 
